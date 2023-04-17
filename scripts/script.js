c79c1eec19b6b1e12fc49c71a8002b9c1b0981f2
José Matheus <XxFULLDLCxX@outlook.com>
2023-04-17 06:48:09 -0300

    Todas as funcionalidade estão funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,28 @@
 // Configuração de Token
 axios.defaults.headers.common["Authorization"] = "26Cc7zGn49HEzmcHoloFlgHq";
 
-
-const login = document.querySelector('.login');
 const chat = document.querySelector('.chat');
-const participants = document.querySelector('.participants');
+const recipient = document.querySelector('.recipient');
+const side_menu = document.querySelector('.side-menu aside');
 const users = document.querySelector('.users');
 let from_username = false;
 let to_username = 'Todos';
 let message_status = 'message';
+let view_status = { 'message': 'publicamente', 'private_message': 'reservadamente' };
 
+const login = document.querySelector('.login');
+
+const getUserName = event => {
+  event.preventDefault();
+  console.log(typeof (from_username));
+  if (typeof (from_username) === 'boolean') {
+    console.log(event.target.username.value);
+    from_username = event.target.username.value;
+    addNewUser(from_username);
+  }
+};
+
+const start = document.querySelector('.start');
 const addNewUser = from_username => {
   axios.post("https://mock-api.driven.com.br/api/vm/uol/participants", { name: from_username })
     .then(response => {
@@ -18,20 +31,10 @@ const addNewUser = from_username => {
       setInterval(sendUserActivity, 5000, from_username);
       setInterval(updateMessages, 3000, from_username);
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
       window.location.reload();
     });
-};
-
-const getUserName = event => {
-  event.preventDefault();
-  console.log(typeof (from_username));
-  if (typeof (from_username) === 'boolean') {
-    from_username = event.target.username.value;
-    console.log(event.target.username.value);
-    addNewUser(from_username);
-  }
 };
 
 const sendUserActivity = from_username => {
@@ -45,22 +48,28 @@ const sendUserActivity = from_username => {
     });
 };
 
+const createRecipientMessage = (to, status) => `Enviando para ${to} (${status})`;
+
+
+const selectParticipant = (element) => {
+  document.querySelectorAll(`.check:not(.view)`).forEach(
+    element => element.classList.add('hidden'));
+  element.querySelector('.check').classList.remove('hidden');
+  to_username = element.textContent.trim();
+  console.log(to_username);
+  recipient.innerHTML = createRecipientMessage(to_username, view_status[message_status]);
+};
+
 const updateParticipants = () => {
   users.innerHTML = '';
   axios.get("https://mock-api.driven.com.br/api/vm/uol/participants")
     .then(response => {
-      [{ name: 'Todos' }, ...response.data].forEach(({ name }) => {
-        console.log(response);
+      response.data.forEach(({ name }, index) => {
+        console.log(response, index);
         const participant = document.createElement('li');
         participant.setAttribute('data-test', 'participant');
         participant.classList.add('y-center');
-        participant.classList.add(name);
-        participant.onclick = () => {
-          document.querySelector(`.${to_username} .check`).classList.add('hidden');
-          to_username = participant.textContent;
-          console.log(to_username);
-          document.querySelector(`.${to_username} .check`).classList.remove('hidden');
-        };
+        participant.onclick = () => selectParticipant(participant);
         if (name !== 'Todos') {
           participant.innerHTML += `<ion-icon name="person-circle-outline"></ion-icon>${name}${check}`;
           users.appendChild(participant);
@@ -73,20 +82,21 @@ const updateParticipants = () => {
     })
     .catch(error => {
       console.log(error, from_username);
-      window.location.reload();
+      // window.location.reload();
     });
 };
 
 const messageVisibility = element => {
-  // erro ao pressionar o mesmo duas vezes.
-  // analise e arrume o participants visto que nomes &¨**$% não pode ser seletores.
-  document.querySelectorAll('.v.check').forEach(element => element.classList.toggle('hidden'));
+  document.querySelectorAll('.view.check').forEach(
+    element => element.classList.add('hidden'));
+  element.querySelector('.view.check').classList.remove('hidden');
   message_status = element.getAttribute('x-state');
+  recipient.textContent = createRecipientMessage(to_username, view_status[message_status]);
 };
 
 const updateMessages = from_username => {
   console.log(typeof (from_username));
-  if (typeof (from_username) === 'string') {
+  if (typeof (from_username) === 'string' || true) {
     axios.get("https://mock-api.driven.com.br/api/vm/uol/messages")
       .then(response => {
         chat.innerHTML = '';
@@ -101,16 +111,21 @@ const updateMessages = from_username => {
           message.setAttribute('data-test', 'message');
           message.classList.add('message');
           message.classList.add(type);
-          if (from !== from_username && to !== from_username && type === 'private_message') {
-            message.classList.add('hidden');
-          }
+
           message.innerHTML = `<span>(${time}) </span><strong> ${from} </strong>${message_types[type]}&nbsp;${text}`;
 
-          chat.appendChild(message);
-          message.scrollIntoView();
-        });
-        console.log(response);
+          reversed = from !== from_username && to !== from_username && to !== 'Todos';
 
+          if (reversed && type === 'private_message') {
+            message.classList.add('hidden');
+          }
+          chat.appendChild(message);
+          start.classList.add('hidden');
+          textarea.focus();
+          if (!reversed) {
+            message.scrollIntoView();
+          }
+        });
       })
       .catch(error => {
         console.log(error);
@@ -119,40 +134,42 @@ const updateMessages = from_username => {
   }
 };
 
-const sendMessage = (event) => {
-  event.preventDefault();
+const sendMessage = () => {
 
-  console.log(from_username, event.target.message.value);
+  console.log(from_username, textarea.value);
   console.log(message_status);
   //Saber o tipo da mensagem, direcionada ou não e privada ou pública
-  axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", {
-    from: from_username,
-    to: to_username,
-    text: event.target.message.value,
-    type: message_status
-  })
-    .then(response => {
-      console.log(response);
-      updateMessages(from_username);
+  if (textarea.value) {
+    axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", {
+      from: from_username,
+      to: to_username,
+      text: textarea.value,
+      type: message_status
     })
-    .catch(error => {
-      console.log(error);
-      //window.location.reload();
-    });
+      .then(response => {
+        console.log(response);
+        updateMessages(from_username);
+      })
+      .catch(error => {
+        console.log(error);
+        //window.location.reload();
+      });
+  }
 };
 
 let loop_menu;
 const overlay = document.querySelector('.overlay');
 
 const viewParticipants = () => {
-  console.log()
-  if (participants.classList.contains('hidden')) {
+  console.log(side_menu);
+  if (overlay.classList.contains('hidden')) {
     updateParticipants();
     loop_menu = setInterval(updateParticipants, 10000);
   } else {
     clearInterval(loop_menu);
   }
-  participants.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+  setTimeout(() => side_menu.classList.toggle('slide-left'), 200);
 };
 
 // menu-content ul li
@@ -160,6 +177,13 @@ const viewParticipants = () => {
 const check = '<ion-icon data-test="check" class="check hidden" name="checkmark"></ion-icon>';
 //message.innerHTML = '<ion-icon name="person-circle-outline"></ion-icon>';
 
+const textarea = document.querySelector('textarea');
+textarea.addEventListener("keydown", event => {
+  if (event.key === "Enter" && !event.shiftKey) {
+    event.preventDefault();
+    sendMessage(event.target.form);
+  }
+});
 
 /*
 Lista de participantes distoantes entre 10 segundos e userActivity há 5 segundos,
@@ -167,7 +191,7 @@ Lista de participantes distoantes entre 10 segundos e userActivity há 5 segundo
  Muito inconclusivo.
  Pode ser só o recarregar da pagina que é necessário, 
  Pode haver a necessidade de criar um array de participants logo no inicio.
- O Avaliador só ver as messages, talvez elas devam está todas com display none... 
+ O Avaliador só ver as messages, talvez elas devam está todas com view none... 
  case sensitive Jm != JM, mas no avaliador, talvez não. 
  a ordem de execução pode inferir no resultado.
  setInterval a ser pode não ser uma boa metódologia.
